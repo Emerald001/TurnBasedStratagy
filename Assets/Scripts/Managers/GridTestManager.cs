bd9bd8558c7eb6920e76953d314a303e489446ad
Andries <andriesdg199@gmail.com>
2023-02-12 11:02:54 +0100

    Chore: Added a Grid Tester

---
+++ Assets/Scripts/Managers/GridTestManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridTestManager : MonoBehaviour
+{
+    private MakeGrid makeGrid;
+    private TurnManager turnM;
+
+    public BattleSettings battleSettings;
+
+    void Start() {
+        turnM = GetComponent<TurnManager>();
+
+        makeGrid = new MakeGrid(
+            turnM, 
+            battleSettings.HexPrefab, 
+            battleSettings.ObstructedHexPrefab, 
+            battleSettings.gridWidth, 
+            battleSettings.gridHeight, 
+            battleSettings.gap, 
+            battleSettings.obstructedCellAmount
+            );
+    }
+}
